18fc8607e3cd3ced3093f5498e080bef25869e4d
Peter Hirn <peter.hirn@buildinformed.com>
2023-09-03 14:17:10 +0200

    Use deps.optimizer.web

--- vitest.config.ts
+++ vitest.config.ts
@@ -4,12 +4,15 @@ import { defineConfig } from "vitest/config";
 export default defineConfig({
   plugins: [solid()],
   test: {
-    deps: {
-      registerNodeLoader: true,
-      inline: [/solid-js/],
-    },
     environment: "jsdom",
     globals: true,
+    deps: {
+      optimizer: {
+        web: {
+          include: ["src/**/*.tsx?"],
+        },
+      },
+    },
     setupFiles: ["./jest-dom-setup.ts"],
   },
   resolve: {
